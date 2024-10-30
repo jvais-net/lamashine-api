54cdf57b6612df93cb5e34fdaa85397cb8426e88
IMTR0J4N <jjorgebeau.pro@gmail.com>
2024-10-30 15:09:11 +0100

    Refactor processMessage function in crisp module: Add error handling and user validation

--- src/api/crisp/services/crisp.js
+++ src/api/crisp/services/crisp.js
@@ -4,79 +4,90 @@ require('dotenv').config();
 
 module.exports = {
     processMessage: async (incomingMessage) => {
-        console.log('Processing incoming message', incomingMessage);
-
-        const { type, origin, content, from, fingerprint, user } = incomingMessage.data;
-        const { nickname, user_id } = user;
-
-        const userExist = await strapi.db.query('api::customer.customer').findOne({
-            where: {
-                id_crisp: user_id
+        try {
+            console.log('Processing incoming message', incomingMessage);
+    
+            const { type, origin, content, from, fingerprint, user } = incomingMessage.data;
+            const { nickname, user_id } = user;
+    
+            // Vérifier que les champs requis sont présents
+            if (!user_id || !nickname) {
+                console.error('User ID or nickname is missing');
+                return;
             }
-        })
-
-        if (!userExist) {
-            await strapi.entityService.create('api::customer.customer', {
-                data: {
-                    id_crisp: user_id,
-                    nickname: nickname
+    
+            // Vérifier si le client existe déjà
+            let dbUser = await strapi.db.query('api::customer.customer').findOne({
+                where: {
+                    id_crisp: user_id
                 }
-            })
-        }
-
-        const dbUser = await strapi.db.query('api::customer.customer').findOne({
-            where: {
-                id_crisp: user_id,
-            }
-        })
-
-        const existMessage = await strapi.db.query('api::message.message').findOne({
-            where: {
-                id_crisp: fingerprint.toString()
-            }
-        })
-
-        if (existMessage) return;
-
-        await strapi.entityService.create('api::message.message', {
-            data: {
-                type: type,
-                customer: dbUser.id,
-                id_crisp: fingerprint.toString(),
-                from: from,
-                origin: origin,
-                content: content,
-            }
-        })
-
-        const tag = content.match(/#(\w+)/g).join('');
-
-        if (tag) {
-
-            if (['#tips', '#nextsteps', '#warnings'].includes(tag)) {
-
-                // @ts-ignore
-                const { OpenAI } = await import('openai')
-                
-                const GPTClient = new OpenAI({
-                    apiKey: process.env.GPT_API_KEY
-                })
-                
-                const response = await GPTClient.chat.completions.create({
-                    messages: [
-                        { role: 'user', content: `Résume ça d'une manière simple à comprendre, courte et précise : ${content.replace(tag, '')}` }
-                    ],
-                    model: 'gpt-4o'
-                });
-
-                await strapi.entityService.create('api::memory.memory', {
+            });
+    
+            // Si le client n'existe pas, le créer
+            if (!dbUser) {
+                dbUser = await strapi.entityService.create('api::customer.customer', {
                     data: {
-                        key: tag.replace('#', ''),
-                        content: response.choices[0].message.content,
-                        customer: dbUser.id
+                        id_crisp: user_id,
+                        nickname: nickname
                     }
-                })
+                });
             }
+    
+            // Vérifier si le message existe déjà
+            const existMessage = await strapi.db.query('api::message.message').findOne({
+                where: {
+                    id_crisp: fingerprint.toString()
+                }
+            });
+    
+            if (existMessage) return;
+    
+            // Créer le message
+            await strapi.entityService.create('api::message.message', {
+                data: {
+                    type: type,
+                    customer: dbUser.id,
+                    id_crisp: fingerprint.toString(),
+                    from: from,
+                    origin: origin,
+                    content: content,
+                }
+            });
+    
+            // Extraire le tag du contenu du message
+            const matches = content.match(/#(\w+)/g);
+            const tag = matches ? matches.join('') : null;
+    
+            if (tag) {
+                if (['#tips', '#nextsteps', '#warnings'].includes(tag)) {
+                    
+                     // @ts-ignore
+                    const { OpenAI } = await import('openai');
+    
+                    const GPTClient = new OpenAI({
+                        apiKey: process.env.GPT_API_KEY
+                    });
+    
+                    const response = await GPTClient.chat.completions.create({
+                        messages: [
+                            { role: 'user', content: `Résume ça d'une manière simple à comprendre, courte et précise : ${content.replace(tag, '')}` }
+                        ],
+                        model: 'gpt-4'
+                    });
+    
+                    await strapi.entityService.create('api::memory.memory', {
+                        data: {
+                            key: tag.replace('#', ''),
+                            content: response.choices[0].message.content,
+                            customer: dbUser.id
+                        }
+                    });
+                }
+            }
+        } catch (error) {
+            console.error('Error processing message:', error);
+            // Vous pouvez également renvoyer une réponse d'erreur appropriée si nécessaire
+            throw error;
         }
     },
 
