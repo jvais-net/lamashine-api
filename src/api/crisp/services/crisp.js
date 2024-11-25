4be73a13133c35bb6c40e05dd1d542da20a16262
IMTR0J4N <jjorgebeau.pro@gmail.com>
2024-11-25 15:51:49 +0100

    Fix property name for conversation verification status in Crisp service

--- src/api/crisp/services/crisp.js
+++ src/api/crisp/services/crisp.js
@@ -31,7 +31,7 @@ module.exports = {
             return;
         }
 
-        console.log((await CrispClient.website.getConversation(process.env.CRISP_WEBSITE_ID, session_id)).isVerified);
+        console.log((await CrispClient.website.getConversation(process.env.CRISP_WEBSITE_ID, session_id)).is_verified);
 
         const isAuthenticated = (await CrispClient.website.getConversationMetas(process.env.CRISP_WEBSITE_ID, session_id)).segments.includes('authentifié');
 
